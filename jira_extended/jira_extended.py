17acc6cb12e8e9a464eda4649fe05f9c1c608a08
Martin Hagstrom <marhag87@gmail.com>
2016-12-18 16:26:00 +0100

    Add customfield_by_name

--- jira_extended/jira_extended.py
+++ jira_extended/jira_extended.py
@@ -27,3 +27,33 @@ def move(self, project=None):
         return True
 
 jira.Issue.move = move
+
+
+def customfield_by_name(self, name):
+    """
+    Get the value of a customfield by name
+    """
+    # Get all fields from Jira. This is expensive, so only do it once
+    if not hasattr(self, '_fields'):
+        response = self._session.get(
+            self._base_url.format(
+                server=self._options['server'],
+                rest_path=self._options['rest_path'],
+                rest_api_version=self._options['rest_api_version'],
+                path='field',
+            ),
+            auth=self._session.auth,
+        )
+        if response.status_code != 200:
+            raise JIRAError(response.text)
+        else:
+            self._fields = response.json()
+
+    for field in self._fields:
+        if field.get('name') == name:
+            break
+    else:
+        raise JIRAError('Could not find customfield')
+    return getattr(self.fields, field.get('id'))
+
+jira.Issue.customfield_by_name = customfield_by_name
